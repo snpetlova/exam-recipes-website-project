e6796036b25bb5591690d2f5cb3c59f36d6ccf8b
Snezhana Petlova <snpetlova@gmail.com>
2023-12-02 17:07:36 +0200

    error handling for create page

--- client/src/pages/CreateRecipe/Create.jsx
+++ client/src/pages/CreateRecipe/Create.jsx
@@ -6,6 +6,7 @@ import { useCookies } from "react-cookie";
 import Button from "react-bootstrap/Button";
 import dishesLeft from "../../assets/dishesLeft.jpg";
 import { useAuth } from "../../context/AuthContext";
+import Alert from "react-bootstrap/Alert";
 import "./Create.css";
 
 function Create() {
@@ -13,11 +14,12 @@ function Create() {
   const { state } = useAuth();
 
   useEffect(() => {
-    // Redirect to login if not authenticated
-    if (!state.isAuthenticated) {
+     // Redirect to login if not authenticated
+    const storedUserId = localStorage.getItem('userId');
+    if (!state.isAuthenticated && !storedUserId) {
       navigate("/login");
     }
-  }, [state.isAuthenticated, navigate]);
+  }, [state.isAuthenticated]);
 
   const userId = getUserId();
 
@@ -30,6 +32,24 @@ function Create() {
     userOwner: userId,
   });
   const [cookies, _] = useCookies(["access_token"]);
+  const [errors, setErrors] = useState({});
+
+  const validateRecipeName = (name) => {
+    return name.length >= 5;
+  };
+
+  const validateIngredients = (ingredients) => {
+    return ingredients.length > 0;
+  };
+
+  const validateInstructions = (instructions) => {
+    return instructions.length >= 20;
+  };
+
+  const validateImageUrl = (url) => {
+    const isValidUrl = /^http(s)?:\/\/\S+\.\S+$/i.test(url);
+    return isValidUrl;
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -49,6 +69,29 @@ function Create() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    const newErrors = {};
+
+    if (!validateRecipeName(recipe.name)) {
+      newErrors.recipeName = "Recipe name should be at least 5 symbols!";
+    }
+
+    if (!validateIngredients(recipe.ingredients)) {
+      newErrors.ingredients = "At least one ingredient is required!";
+    }
+
+    if (!validateInstructions(recipe.instructions)) {
+      newErrors.instructions = "Instructions should be at least 20 symbols!";
+    }
+
+    if (!validateImageUrl(recipe.imageUrl)) {
+      newErrors.imageUrl = "Invalid Image URL!";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
     try {
       await axios.post("http://localhost:3001/recipes", recipe, {
         headers: { authorization: cookies.access_token },
@@ -67,6 +110,26 @@ function Create() {
       </div>
       <div className="rightSide">
         <h3>Create Recipe</h3>
+        {errors.recipeName && (
+        <Alert key="danger" variant="danger" className="alert-danger">
+          <p className="error-message">{errors.recipeName}</p>
+        </Alert>
+      )}
+       {errors.ingredients && (
+        <Alert key="danger" variant="danger" className="alert-danger">
+          <p className="error-message">{errors.ingredients}</p>
+        </Alert>
+      )}
+       {errors.instructions && (
+        <Alert key="danger" variant="danger" className="alert-danger">
+          <p className="error-message">{errors.instructions}</p>
+        </Alert>
+      )}
+       {errors.imageUrl && (
+        <Alert key="danger" variant="danger" className="alert-danger">
+          <p className="error-message">{errors.imageUrl}</p>
+        </Alert>
+      )}
         <form className="createForm" onSubmit={onSubmit}>
           <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" onChange={handleChange} />
