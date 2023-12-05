ec0daea0c0a9d507ccee9285bc0b87145afc5ef7
Snezhana Petlova <snpetlova@gmail.com>
2023-12-05 21:48:08 +0200

    fix bugs

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import './Navbar.css';
+import "./Navbar.css";
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
@@ -22,20 +21,50 @@ export const Navbar = () => {
 
   return (
     <div className="navbar1">
-      <NavLink to="/" className="navbar-refs" activeClassName="active">Home</NavLink>
+      <NavLink to="/" className="navbar-refs" activeClassName="active">
+        Home
+      </NavLink>
 
       {!cookies.access_token ? (
         <>
-          <NavLink to="/login"  className="navbar-refs" activeClassName="active">Login</NavLink>
-          <NavLink to="/register"  className="navbar-refs" activeClassName="active">Register</NavLink>
+          <NavLink to="/login" className="navbar-refs" activeClassName="active">
+            Login
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="navbar-refs"
+            activeClassName="active"
+          >
+            Register
+          </NavLink>
         </>
       ) : (
         <>
-          <NavLink to="/create-recipe"  className="navbar-refs" activeClassName="active">Create Recipe</NavLink>
-          <NavLink to="/my-recipes"  className="navbar-refs" activeClassName="active">My Recipes</NavLink>
-          <NavLink to="/saved-recipes"  className="navbar-refs" activeClassName="active">Saved Recipes</NavLink>
+          <NavLink
+            to="/create-recipe"
+            className="navbar-refs"
+            activeClassName="active"
+          >
+            Create Recipe
+          </NavLink>
+          <NavLink
+            to="/my-recipes"
+            className="navbar-refs"
+            activeClassName="active"
+          >
+            My Recipes
+          </NavLink>
+          <NavLink
+            to="/saved-recipes"
+            className="navbar-refs"
+            activeClassName="active"
+          >
+            Saved Recipes
+          </NavLink>
 
-          <button className="logout" onClick={handleLogout}>Logout</button>
+          <button className="logout" onClick={handleLogout}>
+            Logout
+          </button>
         </>
       )}
     </div>
