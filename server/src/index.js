faa189f8c325d4cd36ece8aac48d3fd788203146
Snezhana Petlova <snpetlova@gmail.com>
2023-12-08 11:45:48 +0200

    deploy to vercel

--- server/src/index.js
+++ server/src/index.js
@@ -2,19 +2,27 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import { userRouter } from './routes/users.js';
-import { recipesRouter } from './routes/recipes.js';
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://flavor-fiesta.vercel.app"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE", "HEAD"],
+    credentials: true,
+  })
+);
 
 app.use(express.json()); //converts data from the front-end to json
 app.use(cors()); //restrict web pages from making requests to a different domain than the one that served the web page
 
-app.use('/auth', userRouter);
-app.use('/recipes', recipesRouter); 
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-const dbUsername = encodeURIComponent("snpetlova")
-const dbPassword = encodeURIComponent("projectPassword123")
+const dbUsername = encodeURIComponent("snpetlova");
+const dbPassword = encodeURIComponent("projectPassword123");
 mongoose.connect(
   `mongodb+srv://${dbUsername}:${dbPassword}@recepies.b2ae3ky.mongodb.net/recepies?retryWrites=true&w=majority`
 );
